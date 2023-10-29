1a296094900d2f1aa13a086f4c430f2fd0ba287d
Nouwan <nouwan@apieceofcheese.net>
2023-10-29 04:21:31 +0100

    Blazor united + htmx with example added.

--- src/api/Api/ExposeNamespace.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Api.Tests")]
-// ReSharper disable once EmptyNamespace Used to make internals visible
-namespace Nouwan.SmeujPlatform.Api
-{
-
-}
--- src/api/Messages.Domain/Message.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Nouwan.SmeujPlatform.Messages.Domain
-{
-    public record Message(long AuthorId, DateTime SendOn, long MessageId, long Id = 0)
-    {
-        public bool IsSaved => Id != 0;
-    }
-}
