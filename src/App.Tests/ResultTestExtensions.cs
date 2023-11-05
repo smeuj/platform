8e2ee70b1824586fece90faa853a6e1aa9d9fda6
Nouwan <18347384+Nouwan@users.noreply.github.com>
2023-11-05 03:38:39 +0100

    #7 added testing.

---
+++ src/App.Tests/ResultTestExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Smeuj.Platform.App.Tests;
+
+public static class ResultTestExtensions {
+    public static TParameter GetParameter<TParameter>(this IResult result) where TParameter : class {
+        
+        var typed = result as RazorComponentResult;
+        foreach (var parameter in typed.Parameters ?? throw new InvalidOperationException()) {
+            if (parameter.Value is TParameter possibleValue) return possibleValue;
+        }
+
+        throw new InvalidOperationException();
+    }
+}
--- src/App/Features/Home/HomeHandler.cs
+++ src/App/Features/Home/HomeHandler.cs
@@ -6,7 +6,6 @@ using Smeuj.Platform.Infrastructure.Database;
 namespace Smeuj.Platform.App.Features.Home;
 
 public interface IHomeHandler {
-
     Task<IResult> GetHomeAsync(CancellationToken ct);
 
     Task<IResult> GetSuggestionsAsync(CancellationToken ct);
@@ -21,34 +20,36 @@ public class HomeHandler : IHomeHandler {
         this.logger = logger;
         logger.LogTrace("HomeHandler;ctor");
     }
-    
+
     public async Task<IResult> GetHomeAsync(CancellationToken ct) {
-        
         var suggestions = await CreateSuggestionsAsync(ct);
         var model = new HomeModel(suggestions);
 
         logger.LogInformation("GetHomeAsync; Returning home with {SuggestionCount} suggestions", suggestions.Length);
         return Components.Home(model);
     }
-    
-    public async Task<IResult> GetSuggestionsAsync(CancellationToken ct) {
 
+    public async Task<IResult> GetSuggestionsAsync(CancellationToken ct) {
         var suggestions = await CreateSuggestionsAsync(ct);
-        logger.LogInformation("GetSuggestionsAsync; Returning suggestions with {SuggestionCount} suggestions", suggestions.Length);
-        
+        logger.LogInformation("GetSuggestionsAsync; Returning suggestions with {SuggestionCount} suggestions",
+            suggestions.Length);
+
         return Components.SmeujList(suggestions);
     }
 
     private async Task<Smeu[]> CreateSuggestionsAsync(CancellationToken ct) {
         const int suggestionCount = 6;
         const int offsetCorrection = suggestionCount - 1;
-        var total = await context.Smeuj.CountAsync(ct) - offsetCorrection;
-        
-        var random = new Random();
-        var offset = random.Next(0, total);
+        var smeujCount = await context.Smeuj.CountAsync(ct);
 
-        logger.LogDebug("GetHomeAsync; Getting {SuggestionCount} Smeu suggestions from the database", suggestionCount);
+        if (smeujCount == 0) return Array.Empty<Smeu>();
+
+        var maxOffset = smeujCount - offsetCorrection;
+        var random = new Random();
+        var offset = random.Next(0, maxOffset);
         var suggestions = await context.Smeuj.Skip(offset).Take(suggestionCount).ToArrayAsync(ct);
+        
+        logger.LogDebug("GetHomeAsync; Getting {SuggestionCount} Smeu suggestions from the database", suggestionCount);
         return suggestions.OrderBy(_ => Guid.NewGuid()).ToArray();
     }
 }
