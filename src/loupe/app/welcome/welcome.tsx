49238176d94290aa61503f6075295dd878d587f6
Nouwan <18347384+Nouwan@users.noreply.github.com>
2025-06-25 00:07:25 +0200

    chagned vite config to correct file type.

--- src/loupe/app/welcome/welcome.tsx
+++ src/loupe/app/welcome/welcome.tsx
@@ -22,7 +22,7 @@ export function Welcome() {
         <div className="max-w-[300px] w-full space-y-6 px-4">
           <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
             <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
-              What&apos;s next?
+              Hello World!
             </p>
             <ul>
               {resources.map(({ href, text, icon }) => (
