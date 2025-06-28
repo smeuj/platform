3e044f68f1e8df481c76393171940d6813a06897
Nouwan <18347384+Nouwan@users.noreply.github.com>
2025-06-29 00:57:51 +0200

    Renamed api to archive

--- src/smeujpedia/App/Pages/Home/IHomeHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Smeuj.Platform.App.Pages.Home;
-
-public interface IHomeHandler {
-    Task<IResult> GetHomeAsync(string? search, CancellationToken ct);
-
-    Task<IResult> GetSuggestionsAsync(CancellationToken ct);
-}
--- src/smeujpedia/Domain/InspirationType.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Smeuj.Platform.Domain;
-
-public enum InspirationType {
-    Author = 0,
-    Text = 1,
-    Url = 2
-}
