df7476e63fac54245a55782ce0fe8ad456ba9a15
Nouwan <18347384+Nouwan@users.noreply.github.com>
2025-06-29 02:16:39 +0200

    Setup api project.

---
+++ src/archive/Api/IHandler.cs
@@ -0,0 +1,8 @@
+﻿using FluentResults;
+
+namespace Smeuj.Archive.Api;
+
+internal interface IHandler<in TParameters, TResult> where TResult : IResultBase {
+
+    public Task<TResult> HandleAsync(TParameters parameters, CancellationToken ct);
+}
